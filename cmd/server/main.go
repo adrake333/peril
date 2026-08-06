a752da8e7347579f14c4d2f40f7dbb8af6a5f3e8
adrake333 <adrake333@gmail.com>
2026-08-06 15:47:52 -0500

    exchanges and queues

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+	"internal/pubsub"
+	"internal/routing"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Fatalf("Error connecting to server: %v", err)
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection successful")
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("Error establishing connection channel: %v", err)
+	}
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Error publishing JSON: %v", err)
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Program is shutting down...")
+
+	return
 }
