6e50e55baae8034760303bf7626ddbd4b2ca8907
adrake333 <adrake333@gmail.com>
2026-08-07 09:52:08 -0500

    wrote DeclareAndBind

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connectionStr := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionStr)
+	if err != nil {
+		log.Fatalf("Error connecting to server: %v", err)
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection successful")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+
 }
---
+++ internal/pubsub/queues.go
@@ -0,0 +1,49 @@
+package pubsub
+
+
+
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+
+
+
+type SimpleQueueType int
+
+const (
+	Durable	SimpleQueueType = iota
+	Transient
+)
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	
+	channel, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("declaring queue: %v", err)
+	}
+
+	isDurable := queueType == Durable
+	isAutoDelete := queueType == Transient
+	isExclusive := queueType == Transient
+
+	queue, err := channel.QueueDeclare(queueName, isDurable, isAutoDelete, isExclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("declaring queue: %v", err)
+	}
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("declaring queue: %v", err)
+	}
+	
+	return channel, queue, nil
+}
