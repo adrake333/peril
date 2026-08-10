6ac108137e7ec0c5a88bd41cdaa95ed6a3bfa435
adrake333 <adrake333@gmail.com>
2026-08-10 16:32:02 -0500

    wrote subscribeJSON

---
+++ internal/pubsub/subscribe.go
@@ -0,0 +1,63 @@
+package pubsub
+
+
+
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+	"context"
+	"encoding/json"
+)
+
+
+
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+	handler func(T),
+) error {
+	ch, queue, err := DeclareAndBind(
+		conn,
+		exchange,
+		queueName,
+		key,
+		queueType,
+	)
+	if err != nil {
+		return err
+	}
+	
+	deliveries, err := ch.Consume(
+		queue.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		for delivery := range deliveries {
+			var target T
+	
+			if err := json.Unmarshal(delivery.Body, &target); err != nil {
+				log.Printf("could not unmarshal: %v", err)
+				continue
+			}
+
+			handler(target)
+
+			delivery.Ack(false)
+		}
+	}()
+
+	return nil
+}
