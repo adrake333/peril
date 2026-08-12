4b61d0e8fbf4b5e34f9a6a6c4412b46791df4c3d
adrake333 <adrake333@gmail.com>
2026-08-12 10:07:36 -0500

    wrote publishGob and publishGameLog

--- cmd/client/war.go
+++ cmd/client/war.go
@@ -17,18 +17,26 @@ import (
 func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.Acktype {
 	return func(rw gamelogic.RecognitionOfWar) pubsub.Acktype {
 		defer fmt.Print("> ")
-		outcome, _, _ := gs.HandleWar(rw)
+		outcome, winner, loser := gs.HandleWar(rw)
 		switch outcome {
 		case gamelogic.WarOutcomeNotInvolved:
 			return pubsub.NackRequeue
+
 		case gamelogic.WarOutcomeNoUnits:
 			return pubsub.NackDiscard
+
 		case gamelogic.WarOutcomeOpponentWon:
+			msg := fmt.Sprintf("%s won a war against %s", winner, loser)
 			return pubsub.Ack
+
 		case gamelogic.WarOutcomeYouWon:
+			msg := fmt.Sprintf("%s won a war against %s", winner, loser)
 			return pubsub.Ack
+
 		case gamelogic.WarOutcomeDraw:
+			msg := fmt.Sprintf("A war between %s and %s resulted in a draw", winner, loser)
 			return pubsub.Ack
+
 		default:
 			log.Println("Error determining war outcome")
 			return pubsub.NackDiscard
--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -5,8 +5,13 @@ package pubsub
 
 import (
 	amqp "github.com/rabbitmq/amqp091-go"
+	"bytes"
 	"context"
+	"encoding/gob"
 	"encoding/json"
+	"time"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 
@@ -17,7 +22,51 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	if err != nil {
 		return err
 	}
-	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: jsonData,})
+	err = ch.PublishWithContext(context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{ContentType: "application/json", Body: jsonData,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var network bytes.Buffer
+	enc := gob.NewEncoder(&network)
+	err := enc.Encode(val)
+	if err != nil {
+		return err
+	}
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/gob",
+			Body: network.Bytes(),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PublishGameLog(ch *amqp.Channel, username, msg string) error {
+	key := routing.GameLogSlug + "." + username
+	gl := routing.GameLog{
+		CurrentTime:	time.Now(),
+		Message:	msg,
+		Username:	username,
+	}
+	err := PublishGob(ch, routing.ExchangePerilTopic, key, gl)
 	if err != nil {
 		return err
 	}
